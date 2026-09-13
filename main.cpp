204fcff9908be77140ba8cdaeea193b997a19a45
pradyun2 <pradyun2@fa26-cs598a-023.cs.illinois.edu>
2026-09-13 13:04:26 -0500

    fix triangle cache construction, OMP added

--- main.cpp
+++ main.cpp
@@ -42,6 +42,7 @@ void set(int i, int j, unsigned char r, unsigned char g, unsigned char b) {
 }
 
 void refresh(Autonoma *c) {
+#pragma omp parallel for
   for (int n = 0; n < H * W; ++n) { // iterate over window bounds
     // each pixel emits a ray -- we should be able to parallelize over rays
     // stupid idea is to just use OpenMP here for naive parallelism
--- src/triangle.cpp
+++ src/triangle.cpp
@@ -47,6 +47,22 @@ Triangle::Triangle(Vector c, Vector b, Vector a, Texture *t)
   thirdX = np.x;
 
   d = -vect.dot(center);
+
+  // need to rerun this, because Plane constructor was dummy'd
+  quadrant_dets[0] = right.x * up.y - up.x * right.y;
+  quadrant_dets[1] = up.x * vect.y - vect.x * up.y;
+  quadrant_dets[2] = vect.x * right.y - right.x * vect.y;
+
+  quadrant_dets[3] = right.y * up.z - up.y * right.z;
+  quadrant_dets[4] = up.y * vect.z - vect.y * up.z;
+  quadrant_dets[5] = vect.y * right.z - right.y * vect.z;
+
+  quadrant_dets[6] = right.z * up.x - up.z * right.x;
+  quadrant_dets[7] = up.z * vect.x - vect.z * up.x;
+  quadrant_dets[8] = vect.z * right.x - right.z * vect.x;
+
+  basis_det = quadrant_dets[0] * vect.z + quadrant_dets[3] * vect.x +
+              quadrant_dets[6] * vect.y;
 }
 
 // replaced w/ the Moller-Trumbore intersection algo
